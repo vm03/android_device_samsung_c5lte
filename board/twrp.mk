d4702c4d10b5934c19c8ab8cea64523c8f3d9c78
vm03 <vasy@vasy.ru>
2017-03-03 16:57:50 +0300

    twrp: add brightness and temp paths and use toolbox

--- board/twrp.mk
+++ board/twrp.mk
@@ -7,3 +7,6 @@ RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TWHAVE_SELINUX := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone4/temp
+TW_USE_TOOLBOX := true
