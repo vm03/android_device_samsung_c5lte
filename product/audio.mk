96dbbd6b66fb12e9ab27736da760d9d9717f9bdc
vm03 <vasy@vasy.ru>
2017-03-22 18:35:32 +0300

    audio: use stock audio hw libs
    For works with TFA9897 amplifier
     * add audio.primary.msm8952.so, libtfa98xx.so and dependenses
     * add shim libs for audio.primary.msm8952.so
     * set permission for tfa devices

--- product/audio.mk
+++ product/audio.mk
@@ -3,7 +3,8 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
-    tinymix
+    tinymix \
+    lib_audio_shim
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
