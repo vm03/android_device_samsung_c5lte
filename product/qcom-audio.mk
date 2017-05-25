80b126db039e83cbdba0b08220869d2ff22b27fa
vm03 <vasy@vasy.ru>
2017-05-25 15:46:22 +0300

    props: audio: add from stock and cleanup dublicate

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -13,8 +13,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     av.streaming.offload.enable=true \
     audio.offload.buffer.size.kb=64 \
     audio.offload.gapless.enabled=true \
-    audio.offload.min.duration.secs=30 \
-    audio.offload.pcm.16bit.enable=false \
+    audio.offload.pcm.16bit.enable=true \
     audio.offload.pcm.24bit.enable=true \
     use.voice.path.for.pcm.voip=true
 
