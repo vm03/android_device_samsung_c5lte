a24b41ba9b7cdac37cbb84d580a1d1cf7db68ebb
vm03 <vasy@vasy.ru>
2017-06-07 15:15:38 +0300

    audio: remove shim

--- product/audio.mk
+++ product/audio.mk
@@ -3,8 +3,7 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
-    tinymix \
-    lib_audio_shim
+    tinymix
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
