8c9b48f0ec055e5e0a0ac3a006f3e3a5f3ac761f
Jesse Hall <jessehall@google.com>
2017-08-29 15:46:29 -0700

    msm8976-common: Declare Vulkan compute level 0 system feature
    Bug: 38396207 (VK_KHR_variable_pointers)
    Bug: 64982680 (maxPerStageDescriptorStorageBuffers >= 16)
    Test: CtsGraphicsTestCases:.VulkanFeaturesTest (ag/2810565)
    Change-Id: I61cde61f53f0e9a1c657e6bd6658a4393360b0a6

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -18,6 +18,7 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/vendor/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:system/vendor/etc/permissions/android.hardware.vulkan.compute.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/vendor/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/vendor/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
 
