b95dcfc8c396acea9e67171dc893f3e6112584fe
Bruno Martins <bgcngm@gmail.com>
2017-12-24 02:08:14 +0000

    msm8976-common: Move keylayout files to vendor
    Change-Id: Idaa0fe01f453afdf2ec419a4c514516e3b2196bd

--- product/keylayouts.mk
+++ product/keylayouts.mk
@@ -1,4 +1,4 @@
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/vendor/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/vendor/usr/keylayout/sec_touchkey.kl
