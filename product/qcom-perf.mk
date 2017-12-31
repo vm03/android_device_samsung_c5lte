61c29375ba114f5ea01e0193650f4dc396d041db
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-31 04:40:40 +0200

    msm8976-common: Build QTI HIDL power 1.0 HAL
    Change-Id: I929a2d3aedf3bfdca962dc34720019d83c7134e1

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -1,8 +1,6 @@
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.msm8952
+    android.hardware.power@1.0-service-qti
 
 # IRQ balance
 PRODUCT_COPY_FILES += \
