4f881c4acc89c5387a7ae6207a2c698cac8d668b
LuK1337 <priv.luk@gmail.com>
2018-01-26 18:12:37 +0100

    msm8976-common: Kill broadcast radio support
    * Our hardware doesn't support it anyway.

--- product/audio.mk
+++ product/audio.mk
@@ -9,8 +9,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
-    android.hardware.broadcastradio@1.0-impl
+    android.hardware.audio.effect@2.0-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:system/vendor/etc/permissions/android.software.midi.xml
