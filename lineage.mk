438b5d88365c958a6781ec2d3b8e17d7921ff178
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-02 19:39:50 -0800

    c5lte: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15

--- lineage.mk
+++ lineage.mk
@@ -2,6 +2,7 @@
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 $(call inherit-product, device/samsung/c5lte/c5lte.mk)
 
 PRODUCT_NAME := lineage_c5lte
