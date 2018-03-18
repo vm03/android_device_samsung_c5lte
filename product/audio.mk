e617934e7272bbc4b549280c410d83d51e4dddc4
Bruno Martins <bgcngm@gmail.com>
2018-03-18 21:20:20 +0000

    msm8976-common: Remove non-existing HAL service
    Change-Id: I26da013dfa359ec8f184a20bc881558be0144b4a

--- product/audio.mk
+++ product/audio.mk
@@ -8,8 +8,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service
+    android.hardware.audio.effect@2.0-impl
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:system/vendor/etc/permissions/android.software.midi.xml
