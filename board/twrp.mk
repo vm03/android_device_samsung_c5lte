4c4cf08d3265696024d2170ad85fc5dd24337e0f
vm03 <vasy@vasy.ru>
2018-08-11 22:44:49 +0300

    twrp: fix crypto

--- board/twrp.mk
+++ board/twrp.mk
@@ -4,11 +4,14 @@ RECOVERY_VARIANT := twrp
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 #TW_TARGET_USES_QCOM_BSP := true
-#TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_DEVICE_DIRS += device/samsung/c5lte/twrp
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TWHAVE_SELINUX := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone4/temp
+TW_EXCLUDE_SUPERSU := true
+TW_EXCLUDE_TWRPAPP := true
 TW_USE_TOOLBOX := true
 endif
--- product/twrp.mk
+++
@@ -1,4 +0,0 @@
-ifeq ($(WITH_TWRP),true)
-PRODUCT_COPY_FILES += \
-    device/samsung/c5lte/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
-endif
