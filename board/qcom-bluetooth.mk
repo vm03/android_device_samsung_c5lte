182cc96cdaab65f0016e043220c13243cf50149f
vm03 <vasy@vasy.ru>
2018-08-12 10:52:13 +0300

    c5lte: bluetooth: include buildcfg

--- board/qcom-bluetooth.mk
+++ board/qcom-bluetooth.mk
@@ -1,3 +1,5 @@
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/c5lte/bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
