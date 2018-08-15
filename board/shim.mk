310b08a47fdf864ba271e2222674455e8b139724
LuK1337 <priv.luk@gmail.com>
2018-08-15 15:48:09 +0200

    c5lte: camera: Add missing Fence destructor
    * Our camera HAL depends on it.
    
    Change-Id: Iedf3dae40e3f2beb284549f486b8721c69837cd0

--- board/shim.mk
+++ board/shim.mk
@@ -1,6 +1,6 @@
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/lib/libcamera_client.so|libcamera_parameters_shim.so \
+    /system/lib/libcamera_client.so|libshims_camera.so \
     /system/vendor/lib64/libflp.so|libshims_flp.so \
     /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
     /system/vendor/lib/libflp.so|libshims_flp.so
