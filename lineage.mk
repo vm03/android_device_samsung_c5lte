2e19d224c1f6160d2c1c1820a83be2f33578577e
vm03 <vasy@vasy.ru>
2018-08-23 18:35:02 +0300

    c5lte: use stock GMS identifiers and fingerprint

--- lineage.mk
+++ lineage.mk
@@ -16,3 +16,10 @@ PRODUCT_BRAND := samsung
 TARGET_VENDOR := samsung
 TARGET_VENDOR_PRODUCT_NAME := C5
 TARGET_VENDOR_DEVICE_NAME := c5lte
+
+# Use the latest approved GMS identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=c5ltezh \
+    PRIVATE_BUILD_DESC="c5ltezh-user 7.0 NRD90M C5000ZHU1BRA1 release-keys"
+
+BUILD_FINGERPRINT := "samsung/c5ltezh/c5ltechn:7.0/NRD90M/C5000ZHU1BRA1:user/release-keys"
