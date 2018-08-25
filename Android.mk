d14b99cecd27193d4d6a4ede9fe464040f721eff
dianlujitao <dianlujitao@lineageos.org>
2018-08-25 19:26:58 +0800

    c5lte: Update WCNSS configuration symlink
     * The new HAL uses /data/vendor/wifi
    
    Change-Id: I2c13fb5fe74d5d4b0e630b2bfc7152da33898784

--- Android.mk
+++ Android.mk
@@ -197,7 +197,7 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_WIDEVINE_SYMLINKS)
 # Create a link for the WCNSS config file, which ends up as a writable
 # # version in /data/misc/wifi
 $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
-    ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
+    ln -sf /data/vendor/wifi/WCNSS_qcom_cfg.ini \
 	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
 endif
