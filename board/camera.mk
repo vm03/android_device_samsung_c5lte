bfccbca03536ea2cb71707a284b97e0d0a9d33f1
Danny Baumann <dannybaumann@web.de>
2018-08-31 13:35:20 +0200

    c5lte: Mark camera-related processes as API level 23.
    Our camera blobs trigger some checks for broken behavior (destroyed
    mutex usage, invalid pthread usage) present in newer API levels.
    
    Change-Id: I8705437e0a5c6d80b86748bff559ce1e8a7bf716

--- board/camera.mk
+++ board/camera.mk
@@ -1,3 +1,5 @@
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
+TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
+    /system/vendor/bin/hw/android.hardware.camera.provider@2.4-service=23
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/c5lte/include
