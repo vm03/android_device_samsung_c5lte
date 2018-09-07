5e441174f11c48f7e3f8952fd8dce5a47d3234cb
vm03 <vasy@vasy.ru>
2018-09-07 09:40:50 +0300

    c5lte: build Snap camera app

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -3,7 +3,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
     android.hardware.camera.provider@2.4-service \
     camera.msm8952 \
-    libshims_camera
+    libshims_camera \
+    Snap
 
 # Permissions
 PRODUCT_COPY_FILES += \
