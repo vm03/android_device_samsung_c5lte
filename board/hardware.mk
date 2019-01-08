2d2876c4741af7012963b112edb1f9d91918d728
Paul Keith <javelinanddart@gmail.com>
2019-01-09 00:47:48 +0100

    c5lte: Build vendor.lineage.touch HAL from hardware/samsung
    Change-Id: I6eca1e9875cb5793a3a45c6e77bc201946ebd897

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,6 +1,5 @@
 # Lineage hardware
 JAVA_SOURCE_OVERLAYS := \
-    org.lineageos.hardware|hardware/samsung/lineagehw|**/*.java \
-    org.lineageos.hardware|device/samsung/c5lte/lineagehw|**/*.java
+    org.lineageos.hardware|hardware/samsung/lineagehw|**/*.java
 
 BOARD_USES_CYANOGEN_HARDWARE := true
--- product/touchscreen.mk
+++ product/touchscreen.mk
@@ -1,3 +1,7 @@
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/vendor/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+
+# Touch features
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.samsung
