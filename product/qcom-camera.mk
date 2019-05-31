3d48ef7520272424b7e08ff80b2235210d48bd9c
vm03 <vasy@vasy.ru>
2019-05-31 10:33:50 +0300

    Revert "Revert "msm8976-common: Binderize camera & graphics composer""
    This reverts commit 868aaf7ad1bc1d950fc70f3140534a4e6e173471.

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,6 +1,7 @@
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.msm8952 \
     libshims_camera \
     Snap
