6c127189982fda6e3c7bd29ab9386396a51b8773
vm03 <vasy@vasy.ru>
2019-07-11 15:30:21 +0300

    Revert "c5lte: Build vendor.lineage.touch HAL from hardware/samsung"
    This reverts commit 785ee98ed5e3f5a75841ee931c98625a98d38685.

--- product/touchscreen.mk
+++ product/touchscreen.mk
@@ -1,7 +1,3 @@
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/vendor/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
-
-# Touch features
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung
